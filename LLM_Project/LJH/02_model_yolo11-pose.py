226450f4d1584a8838751c07b99dea7c75958544
이장헌 <yyahoo1233@naver.com>
2025-03-20 10:03:18 +0900

    lifeislikeanegg

--- LLM_Project/LJH/02_model_yolo11-pose.py
+++ LLM_Project/LJH/02_model_yolo11-pose.py
@@ -1,11 +1,9 @@
 import cv2
-import numpy as np
-import torch
 import tkinter as tk
 from tkinter import filedialog
-import pandas as pd
 import os
 from ultralytics import YOLO
+import json
 
 # TKinter의 GUI 숨기기
 root = tk.Tk()
@@ -30,11 +28,9 @@ if not output_folder:
 # YOLO-Pose 모델 로드
 yolo_model = YOLO("yolo11x-pose.pt")
 
-# cvs 파일 저장을 위한 빈 리스트 생성
-csv_data = []
-
 # 이미지 확장자 지정
-image_extensions = [".jpg", ".jpeg", ".png"]
+## trouble : list로 넣으니 문제가 생김 -> tuple로 변경
+image_extensions = (".jpg", ".jpeg", ".png")
 
 # 폴더 내의 모든 이미지 파일 찾기
 image_paths = []
@@ -48,10 +44,17 @@ if not image_paths:
     print("선택한 폴더 내에 처리할 이미지가 없습니다.")
     exit()
 
+# 결과 이미지 저장 폴더 생성
+image_output_folder = os.path.join(output_folder, 'image')
+json_output_folder = os.path.join(output_folder, 'json')
+os.makedirs(image_output_folder, exist_ok=True)
+os.makedirs(json_output_folder, exist_ok=True)
+
 # 이미지 처리
 for image_path in image_paths:
     # 이미지 파일명 추출
     image_name = os.path.basename(image_path)
+    image_name_no_ext = os.path.splitext(image_name)[0]
     
     # 이미지 로드
     image = cv2.imread(image_path)
@@ -60,38 +63,33 @@ for image_path in image_paths:
         print(f"{image_name}을 불러올 수 없습니다.")
         continue
 
-# YOLO-Pose 모델을 사용하여 포즈 감지
-results = yolo_model(image)
+    # YOLO-Pose 모델을 사용하여 포즈 감지
+    results = yolo_model(image)
 
-# 관절(Keypoints) 좌표 추출 및 시각화
-for result in results:
-    keypoints = result.keypoints.xy.cpu().numpy()  # 좌표 변환
-    scores = result.keypoints.conf.cpu().numpy()  # 신뢰도 변환
+    # Json 파일 저장을 위한 빈 리스트 생성
+    json_data = {'image_name':image_name, 'keypoints':[]}
 
-    for kp, score in zip(keypoints[0], scores[0]):
-            x, y = int(kp[0]), int(kp[1])
-            conf = float(score)
+    # 관절(Keypoints) 좌표 추출 및 시각화
+    for result in results:
+        keypoints = result.keypoints.xy.cpu().numpy()  # 좌표 변환
+        scores = result.keypoints.conf.cpu().numpy()  # 신뢰도 변환
 
-            csv_data.append([image, x, y, conf])
+        for kp, score in zip(keypoints[0], scores[0]):
+                x, y = int(kp[0]), int(kp[1])
+                conf = float(score)
 
-            if conf > 0.5:
-                cv2.circle(image, (x, y), 5, (0, 0, 255), -1)  # 빨간색 점으로 표시
+                json_data['keypoints'].append({"x": x, "y": y, "confidence": conf})
+
+                if conf > 0.5:
+                    cv2.circle(image, (x, y), 5, (0, 0, 255), -1)  # 빨간색 점으로 표시
 
     # 결과 이미지 저장
-    output_image_path = os.path.join(output_folder, f"result_{image_name}")
+    output_image_path = os.path.join(image_output_folder, f"result_{image_name}")
     cv2.imwrite(output_image_path, image)
     print(f"결과 이미지 저장 완료: {output_image_path}")
 
-    # csv 저장
-    csv_file_path = os.path.join(output_folder, "keypoints_result.csv")
-
-# 기존 파일이 있는 경우 추가 저장
-if os.path.exists(csv_file_path):
-    existing_df = pd.read_csv(csv_file_path)
-    new_df = pd.DataFrame(csv_data, columns=["image_name", "x", "y", "confidence"])
-    final_df = pd.concat([existing_df, new_df], ignore_index=True)
-else:
-    final_df = pd.DataFrame(csv_data, columns=["image_name", "x", "y", "confidence"])
-
-final_df.to_csv(csv_file_path, index=False)
-print(f"CSV 파일 저장 완료: {csv_file_path}")
+    # Json 저장
+    json_file_path = os.path.join(json_output_folder, f"{image_name_no_ext}.json")
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        json.dump({"keypoints": json_data}, json_file, indent=4)
+    print(f"Json 저장완료 : {json_file_path}")
